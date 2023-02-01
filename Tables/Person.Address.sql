b5279273769746d962641d703357cbfd3e5a2405
demo1987 <demo1987@gmail.com>
2023-02-01 12:45:30 +0100

    Update Person.Address.sql

--- Tables/Person.Address.sql
+++ Tables/Person.Address.sql
@@ -3,7 +3,7 @@ CREATE TABLE [Person].[Address]
 [AddressID] [int] NOT NULL IDENTITY(1, 1) NOT FOR REPLICATION,
 [AddressLine1] [nvarchar] (60) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [AddressLine2] [nvarchar] (60) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[City] [nvarchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[City] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [StateProvinceID] [int] NOT NULL,
 [PostalCode] [nvarchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [SpatialLocation] [sys].[geography] NULL,
